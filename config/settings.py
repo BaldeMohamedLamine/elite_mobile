72c7797f89b0b090afdd79efcc8634f29a4728c2
BaldeMohamedLamine <mohamedlaminebalde392@gmail.com>
2025-10-23 11:47:50 +0000

    add staticfils

--- config/settings.py
+++ config/settings.py
@@ -244,6 +244,7 @@ LOGGING = {
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
